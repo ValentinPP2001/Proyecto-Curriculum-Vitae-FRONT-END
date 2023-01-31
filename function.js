e85cb2706b14bb6335197ed70d63cedff5d56b94
unknown <paolinivalentin2001@gmail.com>
2023-01-31 14:50:54 -0300

    subiendo primeros archivos

---
+++ function.js
@@ -0,0 +1,10 @@
+document.getElementById('contacto').style.display = 'none';
+
+document.getElementById('boton-contacto').addEventListener('click', function() {
+    document.getElementById('contacto').style.display = 'block';
+})
+
+
+document.getElementById('boton-contacto-ocultar').addEventListener('click',function() {
+    document.getElementById('contacto').style.display = 'none';
+})
